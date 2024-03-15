3bbd32a81d08e214f2cfffab2ef03a152770f856
CharanMN7_pc <maverickid247@gmail.com>
2024-03-15 16:18:22 +0530

    fixed for POST requestss

--- server.js
+++ server.js
@@ -2,35 +2,29 @@ const express = require("express");
 const precautions = require("./precautions.js");
 
 const app = express();
-let personData = {};
-app
-  .route("/precautions/short")
-  .post(async (req, res) => {
-    personData.lat = req.body["lat"];
-    personData.lon = req.body["lon"];
-    personData.pData = req.body["data"];
-    console.log(personData);
-    next();
-  })
-  .get(async (req, res) => {
-    const response = await precautions.short(personData);
-    console.log(req.socket.address());
-    res.send(JSON.parse(response.text()));
-  });
 
-app
-  .route("/precautions/long")
-  .post(async (req, res) => {
-    personData.lat = req.body["lat"];
-    personData.lon = req.body["lon"];
-    personData.pData = req.body["data"];
-    console.log(personData);
-    next();
-  })
-  .get(async (req, res) => {
-    const response = await precautions.long(personData);
-    console.log(req.socket.address());
-    res.send(JSON.parse(response.text()));
-  });
+app.use(express.json());
+
+let personData = {};
+app.post("/precautions/short", async (req, res) => {
+  personData["lat"] = req.body["lat"];
+  personData["lon"] = req.body["lon"];
+  personData["pData"] = req.body["data"];
+  console.log(personData);
+  next();
+  const response = await precautions.short(personData);
+  console.log(req.socket.address());
+  res.send(JSON.parse(response.text()));
+});
+
+app.post("/precautions/long", async (req, res) => {
+  personData["lat"] = req.body["lat"];
+  personData["lon"] = req.body["lon"];
+  personData["pData"] = req.body["data"];
+  console.log(personData);
+  const response = await precautions.long(personData);
+  console.log(req.socket.address());
+  res.send(JSON.parse(response.text()));
+});
 
 app.listen(3000);
